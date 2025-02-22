cde0f95dd226bdeb2d5b15f4e5ab649aa2e758c2
city314 <biyeo126@gmail.com>
2025-02-22 16:49:27 +0700

    Change files

---
+++ src/main/java/com/tt/lap_store/LapStoreApplication.java
@@ -0,0 +1,13 @@
+package com.tt.lap_store;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LapStoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LapStoreApplication.class, args);
+	}
+
+}
