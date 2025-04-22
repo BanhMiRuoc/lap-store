1beeda7d6c662cc2e0d54e60868d8ee36d69e61e
Mai Nguyễn Phương Trang <52200051@student.tdtu.edu.vn>
2025-04-22 22:20:00 +0700

    done model and adding AdminController, UserController

--- lap-store/src/main/java/com/tt/lap_store/Repository/CategoryRepository.java
+++ lap-store/src/main/java/com/tt/lap_store/Repository/CategoryRepository.java
@@ -4,7 +4,11 @@ import com.tt.lap_store.Model.Category;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 public interface CategoryRepository extends JpaRepository<Category, Integer> {
     public abstract boolean existsByName(String name);
+    public List<Category> findByIsActiveTrue(String name);
+
 }
